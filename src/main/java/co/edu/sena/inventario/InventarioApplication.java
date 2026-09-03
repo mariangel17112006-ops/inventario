3b65614cd878dfb818627d453972d29772582c87
mariangel17112006-ops <mariangel17112006@gmail.com>
2026-09-03 10:30:23 -0500

    feat: Configuración de H2 console, JPA y corrección de InventarioApplication

--- src/main/java/co/edu/sena/inventario/InventarioApplication.java
+++ src/main/java/co/edu/sena/inventario/InventarioApplication.java
@@ -1,10 +1,8 @@
+
 package co.edu.sena.inventario;
 
-import org.h2.server.web.JakartaWebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventarioApplication {
@@ -13,10 +11,4 @@ public class InventarioApplication {
         SpringApplication.run(InventarioApplication.class, args);
     }
 
-    @Bean
-    public ServletRegistrationBean<JakartaWebServlet> h2servletRegistration() {
-        ServletRegistrationBean<JakartaWebServlet> registration = new ServletRegistrationBean<>(new JakartaWebServlet());
-        registration.addUrlMappings("/h2-console/*");
-        return registration;
-    }
 }
